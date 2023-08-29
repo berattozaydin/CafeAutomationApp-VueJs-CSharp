ae968a757835340ddc92b2a5e87d5bed1095a58d
Berat <49686676+berattozaydin@users.noreply.github.com>
2023-08-29 09:19:18 +0300

    Update Scaffold-DBContext

---
+++ CafeAutomationDAL/Models/CafeautomationappContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace CafeAutomationDAL.Models;
+
+public partial class CafeautomationappContext : DbContext
+{
+    public CafeautomationappContext()
+    {
+    }
+
+    public CafeautomationappContext(DbContextOptions<CafeautomationappContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseMySQL("Data Source = localhost; Initial Catalog = cafeautomationapp; User ID = root; Password =");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.UserId).HasName("PRIMARY");
+
+            entity.ToTable("user");
+
+            entity.Property(e => e.UserId).HasColumnType("int(11)");
+            entity.Property(e => e.Email).HasMaxLength(50);
+            entity.Property(e => e.FirstName).HasMaxLength(50);
+            entity.Property(e => e.LastName).HasMaxLength(50);
+            entity.Property(e => e.Name).HasMaxLength(50);
+            entity.Property(e => e.Role)
+                .HasDefaultValueSql("'NULL'")
+                .HasColumnType("int(11)");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ CafeAutomationDAL/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CafeAutomationDAL.Models;
+
+public partial class User
+{
+    public int UserId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public int? Role { get; set; }
+}
